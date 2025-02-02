8f875cd8ad833026bd5983754097b5e9d23379ce
aitihyazz <Your GitHub Email>
2025-02-02 22:25:00 +0600

    Current day and time

---
+++ Current date and time.py
@@ -0,0 +1,6 @@
+from datetime import date,time,datetime
+date=date.today()
+time=datetime.now()
+print("Today's date is",date)
+print("\nCurrent date and time is",time)
+print("\nDate componentes ",date.year,"/",date.month,"/",date.day)
